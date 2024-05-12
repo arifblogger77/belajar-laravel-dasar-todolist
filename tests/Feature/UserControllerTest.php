a48e5ad2fbe376921962aefc618708e59bad8a87
arifblogger77 <arifblogger77@gmail.com>
2024-05-12 19:42:19 +0700

    Logout Action

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -34,4 +34,13 @@ class UserControllerTest extends TestCase
             'password' => 'wrong',
         ])->assertSeeText('User or password is wrong');
     }
+
+    public function testLogout()
+    {
+        $this->withSession([
+            'user' => 'khannedy',
+        ])->post('/logout')
+            ->assertRedirect('/')
+            ->assertSessionMissing('user');
+    }
 }
