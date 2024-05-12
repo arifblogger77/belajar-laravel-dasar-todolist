9f6bb20ed7d5d55d6b1bbc7e002a825a60e7899a
arifblogger77 <arifblogger77@gmail.com>
2024-05-12 19:08:40 +0700

    Membuat User Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::view('/template', 'template');
+
+Route::controller(UserController::class)->group(function() {
+    Route::get('/login', 'login');
+    Route::post('/login', 'doLogin');
+    Route::post('/logout', 'doLogout');
 });
